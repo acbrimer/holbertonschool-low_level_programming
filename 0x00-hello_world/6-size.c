7c1ff6fac4e5e61df555d127775282bbd6626c66
Adam Brimer <abrimer@mac.com>
2021-02-04 02:44:53 +0000

    Added solution for problem 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints size of types: char, int, long int, long long int, & float
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int longerIntType;
+	float floatType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longerIntType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+}
