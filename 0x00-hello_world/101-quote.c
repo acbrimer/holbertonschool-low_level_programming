614de92c54c0263eba155dcc3e9dac00cfd9c42a
Adam Brimer <abrimer@mac.com>
2021-02-04 04:02:20 +0000

    Added comment for main method for solution 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * prints quote to stderr using system()
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19' 1>&2");
