89039999adf018632b1479fdcff4e1ea0cfa3551
Adam Brimer <abrimer@mac.com>
2021-02-04 02:46:31 +0000

    Added correct return for solution 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,4 +18,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longerIntType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
 }
