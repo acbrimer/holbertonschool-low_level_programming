b9b682e91d30a87a68ba6a182ab4c1af60a930d8
Adam Brimer <abrimer@mac.com>
2021-02-04 02:17:43 +0000

    Added solution for problem 4. First c filegit add 4-puts.c !

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * prints "Programming is like building a multilingual puzzle
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char quote[52];
+	strcpy(quote, "\"Programming is like building a multilingual puzzle");
+	
+	puts(quote);	
+	return (0);
+}
