c982c156cc9977e810dff7e1cb7e91ec835bfdef
Adam Brimer <abrimer@mac.com>
2021-02-04 02:21:21 +0000

    Corrected errors from betty checker on solution for 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,11 @@
  * prints "Programming is like building a multilingual puzzle
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	char quote[52];
+
 	strcpy(quote, "\"Programming is like building a multilingual puzzle");
-	
-	puts(quote);	
+	puts(quote);
 	return (0);
 }
