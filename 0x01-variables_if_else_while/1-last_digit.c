1edcc44783bc3f12b75cf28e344d2b04881ba7a6
Adam Brimer <abrimer@mac.com>
2021-02-04 23:30:20 +0000

    Shortened variable to fix line>80 char error from betty checker

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-	int n, lastDigit;
+	int n, l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* get last digit remainedr of n/10 */
-	lastDigit = n % 10;
+	l = n % 10;
 	if (lastDigit == 0)
 		printf("Last digit of %d is 0 and is 0\n", n);
 	else if (lastDigit < 6)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
 	else
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n);
 	return (0);
 }
