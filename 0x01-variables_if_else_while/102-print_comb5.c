ed295829dca3a0c1d312a629e0427a79612a3052
Adam Brimer <abrimer@mac.com>
2021-02-05 02:44:05 +0000

    Added solution for advanced problem 13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 100; n++)
+	{
+		int nn;
+
+		for (nn = 0; nn < 100; nn++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((nn / 10) + '0');
+			putchar((nn % 10) + '0');
+			if ((n == 99) && (nn == 99))
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
