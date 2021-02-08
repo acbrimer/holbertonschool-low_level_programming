f7fe063e4dbcf7216f0e6a02bf378c97e64ae254
Adam Brimer <abrimer@mac.com>
2021-02-08 22:56:59 +0000

    Added solution for Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of day as hh:mm
+ * 
+ * Return: void
+*/
+void jack_bauer(void)
+{
+	int hh;
+
+	for (hh = 0; hh < 24; hh++)
+	{
+		int mm;
+
+		for (mm = 0; mm < 60; mm++)
+		{
+			_putchar((hh / 10) + '0');
+			_putchar((hh % 10) + '0');
+			_putchar(':');
+			_putchar((mm / 10) + '0');
+			_putchar((mm % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
