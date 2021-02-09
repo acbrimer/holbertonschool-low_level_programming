6ff489067598fc9a52c0df1009c13f5281ae8cc4
Adam Brimer <abrimer@mac.com>
2021-02-09 01:36:54 +0000

    Added solution for 101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of all multiples of 3 && 5 < 1024
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	int s, i;
+
+	for (i = 1024; i > 0; i--)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			s += i;
+	}
+	printf("%d\n", s);
+
+	return (0);
+}
