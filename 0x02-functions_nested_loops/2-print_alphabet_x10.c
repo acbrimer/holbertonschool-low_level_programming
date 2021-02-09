59ba589a1cebe2a143d5698cf80d681865a661ba
Adam Brimer <abrimer@mac.com>
2021-02-09 06:32:19 +0000

    Fix for 2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,5 @@
 #include "holberton.h"
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet_x10();
-
-	return (0);
-}
-
 /**
  * print_alphabet - prints lowercase a-z followed by \n
  *
