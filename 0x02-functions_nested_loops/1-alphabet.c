7c01776e3703af09240807ea28d428f3e37f2560
Adam Brimer <abrimer@mac.com>
2021-02-09 06:29:43 +0000

    Fix for 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,5 @@
 #include "holberton.h"
 
-void print_alphabet(void);
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-
-	return (0);
-}
 /**
  * print_alphabet - prints lowercase a-z followed by \n
  *
