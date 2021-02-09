6a46553ccdaf69084319b9b3b2027e86c54b4d35
Adam Brimer <abrimer@mac.com>
2021-02-09 00:18:58 +0000

    Added solution for 100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints times table
+ *
+ * Return: void
+*/
+void print_times_table(int n)
+{
+	if ((n < 15) && (n > 0))
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			int ii;
+
+			for (ii = 0; ii <= n; ii++)
+			{
+				int f, m, l, v;
+
+				v = i * ii;
+				f = v / 100;
+				m = v > 99 ? (v % 100) / 10 : v / 10;
+				l = v % 10;
+				if (ii > 0)
+				{
+					_putchar((i * ii) > 99 ? f + '0' : ' ');
+					_putchar((i * ii) > 9 ? m + '0' : ' ');
+		
+				}
+				_putchar(l + '0');
+				if (ii == n)
+				{
+					_putchar('\n');
+					break;
+				}
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
