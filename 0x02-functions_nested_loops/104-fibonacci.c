2c79cb48c963d963bb17c75c6327b15e7c97d69b
Adam Brimer <abrimer@mac.com>
2021-02-10 02:36:18 +0000

    Updated 104-fibonacci to use 2 unsigned longs instead of double/float

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,18 +7,30 @@
 */
 int main(void)
 {
-	float i, a = 1, b = 2, n;
+	unsigned long i = 0;
+	/*m used to split to 2 9-digit max segments */
+	unsigned long m = 1000000000;
+	unsigned long a1, a2, b1, b2, c1, c2;
 
-	printf("1, 2, ");
-	for (i = a; i <= 96; i++)
+	printf("1, 2");
+	a1 = 1 / m;
+	a2 = 1 % m;
+	b1 = 2 / m;
+	b2 = 2 % m;
+	for (i = 3; i < 100; i++)
 	{
-		n = a + b;
-		printf("%.0f", n);
-		if (i == 96)
-			break;
-		printf(", ");
-		a = b;
-		b = n;
+		/*add first 9 digits of c */
+		c1 = (a1 + b1) + ((a2 + b2) / m);
+		/*add last 9 digits of c */
+		c2 = (a2 + b2) % m;
+		if (c1 > 0)
+			printf(", %lu%lu", c1, c2);
+		else
+			printf(", %lu", c2);
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
 	}
 	putchar('\n');
 
