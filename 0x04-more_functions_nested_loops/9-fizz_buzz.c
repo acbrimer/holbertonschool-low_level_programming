dfd21233dee72933706dc2791b478e8896af57be
Adam Brimer <abrimer@mac.com>
2021-02-11 19:32:36 +0000

    Added solution for 9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints Fizz/Buzz/i
+ *
+ * Return: void
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz");
+		if ((i % 5) == 0)
+			printf("Buzz");
+		if ((i % 3) != 0 && (i % 5) != 0)
+			printf("%d", i);
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return 0;
+}
