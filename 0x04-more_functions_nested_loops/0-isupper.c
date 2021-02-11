2963cbe8e12f976f4aed275aa266ff9133f9c48f
Adam Brimer <abrimer@mac.com>
2021-02-11 18:28:04 +0000

    Fixed betty checker error for 0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 /**
  * _isupper- checks for uppercase char
- * @c: The char to checl
+ * @c: The char to check
  *
  * Return: 1 for upper, else 0
 */
 int _isupper(char c)
 {
-	return ((c >= 65) && (c <= 90) ? 1 : 0); 
+	return ((c >= 65) && (c <= 90) ? 1 : 0);
 }
