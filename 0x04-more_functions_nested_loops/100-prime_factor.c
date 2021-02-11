7b0ec63b2791b263845eedcc07ff67c2015903b0
Adam Brimer <abrimer@mac.com>
2021-02-11 20:42:32 +0000

    Added solution for 100-prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints largest prime factor of 612852475143
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	long int m, n = 612852475143;
+
+	/*divide n by 2 until n is odd */
+	while (n % 2 == 0)
+	{
+		m = 2;
+		n = n / 2;
+	}
+	/**
+	 * i counts up odds from 3
+	 * out of odd numbers to check when i = sqrt(n)
+	 */
+	for (long int i = 3; i <= sqrt(n); i = i + 2)
+	{
+		/*check n is evenly divisible by i */
+		if (n % i == 0)
+		{
+			/*i must be prime b/c everything < i alread checked */
+			m = i;
+			n = n / i;
+		}
+	}
+	/*remaining value of n is prime if not 1 or 2 */
+	if (n > 2)
+		m = n;
+	printf("%ld\n", m);
+
+	return (0);
+}
