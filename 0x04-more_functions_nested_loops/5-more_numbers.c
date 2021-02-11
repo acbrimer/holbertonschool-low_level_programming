728ce4b7aeb4a7aa1b212385a984df9f5ea60389
Adam Brimer <abrimer@mac.com>
2021-02-11 18:57:33 +0000

    Fixed 5 to not print the first digit of 15-- moved new line to outer for loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,13 @@ void more_numbers(void)
 	int i, n;
 
 	for (i = 0; i < 10; i++)
-		for (n = 0; n <= 15; n++)
+	{
+		for (n = 0; n < 15; n++)
 		{
 			if (n > 9)
 				_putchar((n / 10) + '0');
-			_putchar((n == 15) ? '\n' : (n % 10) + '0');
+			_putchar((n % 10) + '0');
 		}
+		_putchar('\n');
+	}
 }
