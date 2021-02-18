9c5ad579d791e8450b5fcbb748226ab6e669bc04
Adam Brimer <abrimer@mac.com>
2021-02-18 04:36:24 +0000

    Fix for 100-atoi-- revert back to using limits

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,22 @@
+#include <limits.h>
 /**
  * _atoi - returns signed int from string
  * @s: string w/ int
  *
  * Return: int found in string
 */
+
 int _atoi(char *s)
 {
-	unsigned int i = 0, start, end, num = 0;
-	int started = 0, finished = 0, nth = 1, sign = -1, d;
-	char c;
+	unsigned int i = 0, start, end;
+	int started = 0, finished = 0, num = 0, nth = 1, sign = -1;
 
 	while (finished == 0)
 	{
-		c = *(s + i);
+		char c = *(s + i);
+
 		if (c == '-')
-			sign *= -1;
+			sign = sign == 1 ? -1 : 1;
 		if (c >= '0' && c <= '9' && started == 0)
 		{
 			start = i;
@@ -31,12 +33,17 @@ int _atoi(char *s)
 		return (0);
 	for (i = end; i >= start; i--)
 	{
-		d = *(s + i) - 48;
-		num += (*(s + i) - 48) * nth;
+		int d = *(s + i) - 48;
+
+		if (sign == -1 && ((d - 48) * nth) > INT_MAX + num)
+			return (0);
+		else if (sign == 1 && ((d * nth) * -1) < INT_MIN - num)
+			return (0);
+		num -= (*(s + i) - 48) * nth;
 		nth *= 10;
 		if (i == 0)
 			break;
 	}
 
-	return (sign == -1 ? -num : num);
+	return (num * sign);
 }
