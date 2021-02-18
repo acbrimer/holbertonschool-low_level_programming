24e385be08fd431a9e195ec0665241a0476b87e6
Adam Brimer <abrimer@mac.com>
2021-02-18 16:56:17 +0000

    Added solution for 6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+/**
+ * cap_string - converts first char of words to upper
+ * @s: input string to convert
+ *
+ * Return: pointer to input string
+*/
+
+char *cap_string(char *s)
+{
+	int i = 1;
+	char prev;
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 97 && *(s + i) <= 122)
+		{
+		switch (prev)
+		{
+			case ' ':
+			case '	':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				*(s + i) = *(s + i) - 32;
+			default:
+				*(s + i) = *(s + i);
+		}
+		}
+		prev = *(s + i);
+		i++;
+	}
+
+	return (s);
+}
