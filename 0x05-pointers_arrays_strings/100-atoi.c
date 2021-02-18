0aa36408dcf259129cea604e046089f5ee43dfc1
Adam Brimer <abrimer@mac.com>
2021-02-18 04:28:11 +0000

    Fix for 100-atoi-- removed limits import & removed int limit check

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,20 +4,17 @@
  *
  * Return: int found in string
 */
-
-#include <limits.h>
-
 int _atoi(char *s)
 {
 	unsigned int i = 0, start, end;
-	int started = 0, finished = 0, num = 0, nth = 1, sign = -1;
+	int started = 0, finished = 0, num = 0, nth = 1, sign = -1, d;
+	char c;
 
 	while (finished == 0)
 	{
-		char c = *(s + i);
-
+		c = *(s + i);
 		if (c == '-')
-			sign = sign == 1 ? -1 : 1;
+			sign *= -1;
 		if (c >= '0' && c <= '9' && started == 0)
 		{
 			start = i;
@@ -34,12 +31,7 @@ int _atoi(char *s)
 		return (0);
 	for (i = end; i >= start; i--)
 	{
-		int d = *(s + i) - 48;
-
-		if (sign == -1 && ((d - 48) * nth) > INT_MAX + num)
-			return (0);
-		else if (sign == 1 && ((d * nth) * -1) < INT_MIN - num)
-			return (0);
+		d = *(s + i) - 48;
 		num -= (*(s + i) - 48) * nth;
 		nth *= 10;
 		if (i == 0)
