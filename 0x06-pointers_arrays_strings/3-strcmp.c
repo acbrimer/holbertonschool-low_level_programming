b16a6ccbbe212ad01ce0e9c01604279a5b523230
Adam Brimer <abrimer@mac.com>
2021-02-18 16:20:31 +0000

    Added solution for 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Returns: int difference between first mismatchd char
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) || *(s2 + i))
+	{
+		if (*(s1 + i) != *(s2 + i))
+			return (*(s1 + i) - *(s2 + i));
+		i++;
+	}
+
+	return (0);
+}
