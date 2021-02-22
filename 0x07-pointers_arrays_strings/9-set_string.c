c04375b468938efe404f64781e0a9b98279ea85b
Adam Brimer <abrimer@mac.com>
2021-02-22 21:41:32 +0000

    Fix for 9-set_string-- solution w/out malloc

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-
 /**
  * set_string - sets the value of pointer to char
  * @s: pointer to pointer for value
@@ -9,6 +6,5 @@
 
 void set_string(char **s, char *to)
 {
-	s[0] = malloc(strlen(to) + 1);
 	s[0] = to;
 }
