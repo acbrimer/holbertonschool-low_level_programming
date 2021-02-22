5df7d40b5b4945fb17fb1dd3b27c94b2c9a7d032
Adam Brimer <abrimer@mac.com>
2021-02-22 17:34:46 +0000

    Added solution for 5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+/**
+ * _strstr - finds string within string
+ * @haystack: string to search
+ * @needle: string to match
+ *
+ * Return: pointer to beginning of substring or null
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int hi = 0, ni = 0;
+
+	while (haystack[hi])
+	{
+		ni = haystack[hi] == needle[ni] ? ni + 1 : 0;
+		if (!needle[ni])
+			return (haystack + (hi + 1 - ni));
+		hi++;
+	}
+
+	return (0);
+}
