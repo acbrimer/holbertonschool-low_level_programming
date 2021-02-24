312fab5c27358a5837fffab9da451f7bb958f723
Adam Brimer <abrimer@mac.com>
2021-02-24 23:49:15 +0000

    Added solution for 5-sqrt_recursion using helper method for recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+/**
+ * _sqrt - recursive function to get square root
+ * @n: number to find square root of
+ * @x: current number to test for square root of n
+ *
+ * Return: square root of x or -1
+*/
+int _sqrt(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+	if (x * x > n)
+		return (-1);
+	return (_sqrt(n, x + 1));
+}
+
+/**
+ * _sqrt_recursion - function to handle call to recursive _sqrt
+ * @x: number to find square root of
+ *
+ * Return: int square root of x
+*/
+int _sqrt_recursion(int n)
+{
+	return (n < 0 ? -1 : _sqrt(n, 1));
+}
