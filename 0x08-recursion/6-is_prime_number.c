27ef114d32ba398b9bab55b7b5cd57c00fda7b47
Adam Brimer <abrimer@mac.com>
2021-02-25 00:32:25 +0000

    Added solution for 6-is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+/**
+ * is_divisible - recursive check if n % x = 0
+ * @n: number to check
+ * @x: number to test
+ *
+ * Return: 1 if prime, 0 if not prime
+*/
+int is_divisible(int n, int x)
+{
+	if (n % x == 0)
+		return (0);
+	if (x > (n / 2))
+		return (1);
+	return (is_divisible(n, x + 1));
+}
+
+/**
+ * is_prime_number - checks if number is prime
+ * @n: number to check
+ *
+ * Return: 1 for prime, 0 for non-prime/negative
+*/
+int is_prime_number(int n)
+{
+	return (n < 0 ? 0 : is_divisible(n, 2));
+}
