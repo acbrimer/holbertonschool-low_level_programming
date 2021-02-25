17732535884fb30999635bcc139a4156dede31d7
Adam Brimer <abrimer@mac.com>
2021-02-25 01:07:09 +0000

    Added solution for 7-is_palindrome

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,32 @@
+/**
+ * palindrome_checker - recursive function to compare chars
+ * @s: string to check
+ * @l: string length
+ * @i: current recursive loop
+ *
+ * Return: 1 if s is palindrome, else 0
+*/
+int palindrome_checker(char *s, int l, int i)
+{
+	if (s[i] != s[l - 1 - i])
+		return (0);
+	if (i >= l / 2)
+		return (1);
+	return (palindrome_checker(s, l, i + 1));
+}
+
+/**
+ * is_palindrome - gets length & calls recursive to determine if palindrome
+ * @s: string to check
+ *
+ * Return: 1 if s is palindrome, else 0
+*/
+int is_palindrome(char *s)
+{
+	int l = 0;
+
+	while (s[l])
+		l++;
+
+	return (l == 0 ? 1 : palindrome_checker(s, l, 0));
+}
