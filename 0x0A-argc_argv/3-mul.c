d998c835f275a32b10869f398a91ac2666b832ca
Adam Brimer <abrimer@mac.com>
2021-03-01 00:20:14 +0000

    Added solution for 3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * isnumeric- checks if string is numeric
+ * @s: string to check
+ *
+ * Return: 1 for numeric, 0 for non-numeric
+*/
+int isnumeric(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if ((i == 0 && s[i] == '-') || isdigit(s[i]))
+			i++;
+		else
+			return (0);
+	}
+
+	return (1);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of args (unused)
+ * @argv: argv[1] for fist number, argv[2] for second number
+ *
+ * Return: always 0
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+	if (isnumeric(argv[1]) && isnumeric(argv[2]))
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		puts("Error");
+
+	return (0);
+}
