75b3fb1e8142959351f3a6e530dff3d4379d9049
Adam Brimer <abrimer@mac.com>
2021-03-02 15:45:12 +0000

    Initial commit for 0x0B with solution for task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of size = size filled with char c
+ * @size: size of array
+ * @c: char to fill
+ *
+ * Return: new char* pointer to array
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *r = (char *)malloc(size);
+
+	if (size == 0)
+		return (0);
+	for (i = 0; i < size; i++)
+		r[i] = c;
+
+	return (r);
+}
