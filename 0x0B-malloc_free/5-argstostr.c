b8c3c8d019c6982f19f06a1a6f699a1271670553
Adam Brimer <abrimer@mac.com>
2021-03-02 23:57:27 +0000

    Fix for 5-argstostr-- casting malloc

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -18,7 +18,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (a = 0; a < ac; a++)
 		s += sizeof(av[a]);
-	str = malloc(s);
+	str = (char *)malloc(s);
 	if (str == NULL)
 		return (NULL);
 	for (a = 0; a < ac; a++)
