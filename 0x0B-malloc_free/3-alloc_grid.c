e64268002fa7cff022178b1896ec25f5518a5c34
Adam Brimer <abrimer@mac.com>
2021-03-02 17:30:03 +0000

    Added solution for 3-alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - cretes width x height 2D array
+ * @width: size of inner array
+ * @height: size of outer array
+ *
+ * Return: 2D array pointer to grid
+*/
+int **alloc_grid(int width, int height)
+{
+	int *r, **g;
+	int i, ii, s;
+
+	s = (sizeof(int *) * height) + (sizeof(int) * width * height);
+	g = (int **)malloc(s);
+	/* get first row in g */
+	r = (int *)(g + height);
+	/* loop to set rows in g */
+	for (i = 0; i < height; i++)
+		g[i] = (r + width * i);
+	/* loop to set value at g[i][ii] */
+	for (i = 0; i < height; i++)
+		for (ii = 0; ii < width; ii++)
+			g[i][ii] = 0;
+
+	return (g);
+}
