260b7b1ddaf9fe90db96514fcec1620b281a196e
Adam Brimer <abrimer@mac.com>
2021-03-03 05:50:08 +0000

    Added first attempt for 100-strtow

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _charcount - counts non-space chars in string
+ * @str: input string
+ *
+ * Return: int count of non-space chars
+*/
+int _charcount(char *str)
+{
+	int i, cc = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (str[i] != ' ')
+			cc++;
+
+	return (cc);
+}
+
+/**
+ * _strtok - returns substring until space delim
+ * @str: full string to check
+ * @start: starting index in string
+ *
+ * Return: next string until delim
+*/
+char *_strtok(char *str, int start)
+{
+	int i, l = 0;
+	char *s = NULL;
+
+	while (str[l + start] != ' ')
+		l++;
+	s = malloc((l + 1) * sizeof(char));
+	for (i = 0; i < l; i++)
+		s[i] = str[i + start];
+	s[l] = '\0';
+
+	return (s);
+}
+
+/**
+ * strtow - splits string into array of strings
+ * @str: string to split
+ *
+ * Return: char ** array of strings
+*/
+char **strtow(char *str)
+{
+	int cc = 0, i = 0, r = 0;
+	char **res = NULL;
+	char *tmp;
+
+	cc = _charcount(str);
+	res = malloc(sizeof(char *) * cc);
+	while (str[i])
+	{
+		if (str[i] == ' ')
+			i++;
+		else
+		{
+			int l = 0;
+
+			tmp = _strtok(str, i);
+			printf("i: %d tmp: %s\n", i, tmp);
+			while (tmp[l])
+				l++;
+			res[r] = tmp;
+			r++;
+			i += l;
+		}
+	}
+
+	return (res);
+}
