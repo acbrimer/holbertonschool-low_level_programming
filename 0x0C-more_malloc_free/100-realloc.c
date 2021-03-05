a466bc009f5f82f2546924cbab578298b395b66d
Adam Brimer <abrimer@mac.com>
2021-03-05 05:54:41 +0000

    Fix for 100-realloc-- added free(ptr) before return NULL for malloc fail

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -42,7 +42,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	nptr = malloc(new_size);
-	if (ptr == NULL)
+	if (ptr == NULL || old_size == 0)
 	{
 		free(ptr);
 		return (nptr);
