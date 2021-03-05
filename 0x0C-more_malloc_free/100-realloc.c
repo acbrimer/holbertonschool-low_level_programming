812f69ff6bf4709a9b58a85993f27155db0e646a
Adam Brimer <abrimer@mac.com>
2021-03-05 05:44:28 +0000

    Fix for 100-realloc-- added condition for new_size == 0

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,13 +34,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *nptr;
 	unsigned int s;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
-		return (ptr);
 	nptr = malloc(new_size);
 	if (ptr == NULL)
 		return (nptr);
