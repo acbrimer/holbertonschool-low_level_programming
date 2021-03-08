a54842ffd541b248f265f2e1e071f0b1e51e4db9
Adam Brimer <abrimer@mac.com>
2021-03-08 21:12:31 +0000

    Fix for task 0-- added struct description comment and fixed betty error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-struct dog 
+/**
+ * struct dog - man's best struct
+ * @name: dog's name
+ * @age: dog's age (in people years?)
+ * @owner: dog's human
+ *
+ * Description: holds information about a dog
+*/
+struct dog
 {
 	char *name;
 	float age;
