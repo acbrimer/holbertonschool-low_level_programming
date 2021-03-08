7da49830abd50e81fc9643419684cc78176b8a2f
Adam Brimer <abrimer@mac.com>
2021-03-08 21:20:16 +0000

    Added solution for 1-init_dog. Updated dog.h header

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - inits a new var of type struct dog
+ * @d: dog struct to init
+ * @name: name for dog struct
+ * @age: age for dog struct
+ * @owner: owner for dog struct
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
