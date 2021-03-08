cb48d5ffdb4a1a95b6630c10f80560a8c9a64df5
Adam Brimer <abrimer@mac.com>
2021-03-08 21:26:41 +0000

    Fix for 1-init_dog-- added condition to handle NULL dog struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name == NULL ? "" : name;
-	d->age = age;
-	d->owner = owner == NULL ? "" : owner;
+	if (d != NULL)
+	{
+		d->name = name == NULL ? "" : name;
+		d->age = age;
+		d->owner = owner == NULL ? "" : owner;
+	}
 }
