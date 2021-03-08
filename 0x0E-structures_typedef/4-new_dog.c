2f05f86791f222cb5a1bd6259309770ede5ce990
Adam Brimer <abrimer@mac.com>
2021-03-08 22:23:44 +0000

    Fix for 4-new_dog-- added _strcpy method to set string members for dog_t

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,31 @@
 #include "dog.h"
 
 /**
+ * _strdup - copies string using malloc
+ * @str: string to copy
+ *
+ * Return: char * pointer to copied string
+*/
+char *_strdup(char *str)
+{
+	int l = 0, i;
+	char *n = NULL;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[l])
+		l++;
+	n = (char *)malloc(l + 1);
+	if (n == NULL)
+		return (NULL);
+	for (i = 0; i < l; i++)
+		n[i] = str[i];
+	n[l] = '\0';
+
+	return (n);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: dog's name
  * @age: dog's age
@@ -16,8 +41,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 	dog->age = age;
-	dog->name = name == NULL ? "(nil)" : name;
-	dog->owner = owner == NULL ? "(nil)" : owner;
+	if (name != NULL)
+		dog->name = _strdup(name);
+	if (owner != NULL)
+		dog->owner = _strdup(owner);
 
 	return (dog);
 }
