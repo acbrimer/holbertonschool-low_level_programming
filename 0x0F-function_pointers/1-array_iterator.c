eea3645ca5492f8e2ce01f01d33c4edda577a3ff
Adam Brimer <abrimer@mac.com>
2021-03-10 23:10:18 +0000

    Added solution for 1-array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * array_iterator - executes function for each element of array
+ * @array: array of ints to pass as input to function
+ * @size: size of array
+ * @action: function to execute
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action != NULL)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
