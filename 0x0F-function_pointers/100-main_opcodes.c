1ad17c35f8da619244c8348331397d92afa99fd9
Adam Brimer <abrimer@mac.com>
2021-03-11 03:03:08 +0000

    Added solution for 100-main_opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcode
+ * @argc: number of args
+ * @argv: value of args
+ *
+ * Return: 0 for success, 1-2 for error
+*/
+int main(int argc, char **argv)
+{
+	unsigned int *main_ptr = (unsigned int *)main;
+	int i, size;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	size = atoi(argv[1]);
+	if (size < 0)
+	{
+		puts("Error");
+		return (2);
+	}
+	/* derefrenced pointer & 0xFF to get last sig. byte */
+	printf("%02x", *main_ptr & 0xFF);
+	for (i = 1; i < size; i++)
+	{
+		printf(" %02x", main_ptr[i] & 0xFF);
+	}
+	putchar('\n');
+
+	return (0);
+}
