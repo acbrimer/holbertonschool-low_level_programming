8f0d7216f71f4cdb9ac984f6d7312439bd9af0cc
Adam Brimer <abrimer@mac.com>
2021-03-11 21:38:53 +0000

    Fix for 3-print_all-- changed float print back to %f and changed int to %i

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -38,10 +38,10 @@ void print_all(const char * const format, ...)
 					printf("%s%c", _SEPARATOR(i, ", "), va_arg(a, int));
 					break;
 				case 'i':
-					printf("%s%d", _SEPARATOR(i, ", "), va_arg(a, int));
+					printf("%s%i", _SEPARATOR(i, ", "), va_arg(a, int));
 					break;
 				case 'f':
-					printf("%s%.6g", _SEPARATOR(i, ", "), va_arg(a, double));
+					printf("%s%f", _SEPARATOR(i, ", "), va_arg(a, double));
 					break;
 				case 's':
 					printf("%s%s", _SEPARATOR(i, ", "), _sarg(va_arg(a, char*)));
