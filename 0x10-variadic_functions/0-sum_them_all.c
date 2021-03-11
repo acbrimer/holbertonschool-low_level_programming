55cfd20072df4fb37eef78548191ab1802e73cf2
Adam Brimer <abrimer@mac.com>
2021-03-11 19:40:43 +0000

    Initial commit for 0x10. Added solution for 0-sum_them_all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all input params
+ * @n: first param
+ *
+ * Return: int sum of params
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i = 0, p;
+
+	va_start(args, n);
+	for(p = (int)n; p; p--)
+		i += va_arg(args, unsigned int);
+	va_end(args);
+
+	return (i);
+}
