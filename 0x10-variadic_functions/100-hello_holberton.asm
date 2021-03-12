f8ec9e1946f08fec6c8d911d167ac83ea44e231d
Adam Brimer <abrimer@mac.com>
2021-03-12 04:36:51 +0000

    Fix for 100-hello_holberton

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,33 +1,20 @@
-extern printf 
+segment .data                   ;section declaration
+    msg db     "Hello, Holberton", 0xA, 0x0 ;our NULL-terminated string, ending with 0xA (lf) or 0xD (CR)
+    len equ     $ - msg             ;length of our dear string
+
+segment .text                   ;section declaration
+    global  main            ;we must export the entry point to the ELF linker or loader
+main:
+    enter 0,0
+    pusha
+                                ;write our string to stdout
+    mov     edx,len             ;third argument: message length
+    mov     ecx,msg             ;second argument: pointer to message to write
+    mov     ebx,1               ;first argument: file handle (stdout)
+    mov     eax,4               ;system call number (sys_write)
+    int     0x80                ;call kernel
  
-segment .data 
-    msg db     "Hello, Holberton", 0x0 
-    len equ     $ - msg 
-    formatstr: 
-              dd formatstring 
- 
-segment .rodata 
-    formatstring: db 25H, 73H, 0AH, 00H   ; "%s\n" 
- 
- 
-segment .text                   ;section declaration 
-    global  main 
- 
-printstring:; Function int printstring(char*) 
-    push    dword [esp + 4] ; char* 
-    push    dword [formatstr] 
-    call    printf  ; printf("%s\n", char*) 
-    add     esp, 8 
-    ret 
- 
-main: 
-    enter 0,0 
-    pusha 
-                                ;write our string to stdout 
-    push msg 
-    call printstring 
- 
-    popa 
-    xor eax, eax  ; set eax 0 
-    leave 
-    ret 
+    popa
+    xor eax, eax  ; set eax 0
+    leave
+    ret
