a8981fef90fac685ee97dd780b3431317a902800
Adam Brimer <abrimer@mac.com>
2021-03-19 02:43:59 +0000

    Fixes for 2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,15 +10,17 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
+	
 	list_t *new = malloc(sizeof(list_t));
-	list_t *tmp = head[0];
-
+	list_t *tmp = *head;
+	
 	if (new == NULL)
 		return (NULL);
+	new->str = malloc(sizeof(str));
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = tmp;
-	head[0] = new;
+	*head = new;
 
-	return (new);
+	return (*head);
 }
