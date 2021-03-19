921ec7133dca8bb9b62021987584f7cd45b230e8
Adam Brimer <abrimer@mac.com>
2021-03-19 02:29:57 +0000

    Fixed betty errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,12 +5,16 @@
  * add_node - addes node with str value to beginning of linked list
  * @head: existing linked list
  * @str: string value for new node in list
+ *
+ * Return: address of added list element
 */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 	list_t *tmp = head[0];
 
+	if (new == NULL)
+		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = tmp;
