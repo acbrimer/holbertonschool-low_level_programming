52b38237501de288c679652b838c0daf1d9b0a6d
Adam Brimer <abrimer@mac.com>
2021-03-19 02:05:33 +0000

    Added description for list_len and fixed whitespace error

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
+/**
+ * list_len - gets the length of a linked list from start node h
+ * @h: head node to count from
+ *
+ * Return: size_t of linked list
+*/
 size_t list_len(const list_t *h)
 {
 	const list_t *tmp = h;
 	size_t s;
-	
+
 	for (s = 0; tmp != NULL; s++)
 		tmp = tmp->next;
 
