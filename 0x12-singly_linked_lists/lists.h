aa80c879d301af2ac73416ac366fa82c601c4863
Adam Brimer <abrimer@mac.com>
2021-03-19 01:58:06 +0000

    Changed size_t to unsigned int in list_s struct

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,16 +5,19 @@
 int _putchar(char c);
 
 /**
- * struct list_t - a node for a singly linked list
- * @str: string data within the node
- * @len: size_t of str
- * @next: pointer to next list_t node or NULL for last node
-*/
-typedef struct list_t
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
 {
 	char *str;
-	size_t len;
-	struct list_t *next;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
