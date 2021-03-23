d4b396e14eb6334fc32a1504e8a77ddb50676f20
Adam Brimer <abrimer@mac.com>
2021-03-23 19:24:20 +0000

    Fix for task 9-- changed for loop to while

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,12 +16,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new == NULL)
 		return (NULL);
-	for (i = 0; i < idx && current != NULL; i++)
-		current = current->next;
-	if (i < idx)
+	while (i < idx - 1)
 	{
-		free(new);
-		return (NULL);
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		current = current->next;
+		i++;
 	}
 	new->next = current->next;
 	current->next = new;
