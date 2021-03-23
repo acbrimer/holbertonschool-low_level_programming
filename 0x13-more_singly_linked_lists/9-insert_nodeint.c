5541246380aab4760a89d9a5c014b1ea3c333513
Adam Brimer <abrimer@mac.com>
2021-03-23 19:30:59 +0000

    Fix for task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,11 +12,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *current = *head;
 	listint_t *new = malloc(sizeof(listint_t));
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	if (new == NULL)
 		return (NULL);
-	while (i < idx - 1)
+	while (i < idx)
 	{
 		if (current == NULL)
 		{
