206c007346a16684953c1c63e3d76ddc739339fc
Adam Brimer <abrimer@mac.com>
2021-03-23 16:43:28 +0000

    Added solution for task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees all nodes of listint_t list
+ * @head: list of head node to free
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head, *next;
+
+	while (tmp)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	*head = NULL;
+}
