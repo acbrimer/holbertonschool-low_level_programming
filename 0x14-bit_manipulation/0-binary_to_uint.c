3f01ed53480f9bea034a201ba59ec83846894980
Adam Brimer <abrimer@mac.com>
2021-03-25 21:04:42 +0000

    Fix for task 0-- handling NULL string

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  * binary_to_uint - converts binary string to unsigned int
@@ -10,6 +11,8 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int d = 0;
 
+	if (b == NULL)
+		return (0);
 	while (*b)
 	{
 		if (!(*b == '0' || *b == '1'))
