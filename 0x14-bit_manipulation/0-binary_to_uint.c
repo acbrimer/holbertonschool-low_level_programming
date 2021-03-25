09a9ca06de0039d7fe6f5470c3d9a4d83ed947ad
Adam Brimer <abrimer@mac.com>
2021-03-25 21:02:51 +0000

    Added solution for 0-binary_to_uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts binary string to unsigned int
+ * @b: binary string pointer
+ *
+ * Return: unsigned int base 10 value of b or 0 if failed to convert
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int d = 0;
+
+	while (*b)
+	{
+		if (!(*b == '0' || *b == '1'))
+			return (0);
+		d *= 2;
+		if (*b++ == '1')
+			d++;
+	}
+
+	return (d);
+}
