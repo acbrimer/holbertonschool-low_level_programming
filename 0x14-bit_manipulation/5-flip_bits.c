10bfe2e4353eb7c7fb541d584baf4f75419d7342
Adam Brimer <abrimer@mac.com>
2021-03-25 23:49:46 +0000

    Added solution for 5-flip_bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - counts unequal bit values between n and m
+ * @n: number 1
+ * @m: number 2
+ *
+ * Return: count of mismatched bits between m and n
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int b = 0, i;
+
+	for (i = 0; i < 63; i++)
+		if (((n >> i) & 1) != ((m >> i) & 1))
+			b++;
+
+	return (b);
+}
