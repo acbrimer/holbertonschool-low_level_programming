c9af869ac3b266b0dc3ab686feabc5a8586a3092
Adam Brimer <abrimer@mac.com>
2021-03-30 19:29:54 +0000

    Fix for task 0-- return 0 on fd == -1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd = -1)
+	if (fd == -1)
 		return (0);
 	while ((buflen = read(fd, buf, 1024)) > 0)
 	{
