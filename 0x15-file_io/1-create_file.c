84ec5f0ea427666386cd3425efba2c439a5341a4
Adam Brimer <abrimer@mac.com>
2021-03-30 21:20:49 +0000

    Fix for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,16 +14,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content)
 		while (text_content[len])
 			len++;
 	text = text_content ? text_content : "";
-	writelen = write(fd, text, len + 1);
+	writelen = write(fd, text, len);
 	close(fd);
-	if (writelen != len + 1)
+	if (writelen != len)
 		return (-1);
 
 	return (1);
