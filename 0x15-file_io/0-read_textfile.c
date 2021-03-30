15ff9299c8462a4838ba689bac52485688b92f1f
Adam Brimer <abrimer@mac.com>
2021-03-30 20:11:34 +0000

    Fix for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		close(fd);
+	{
+		free(buf);
+		return (0);
+	}
 	buflen = read(fd, buf, letters);
 	if (buflen <= 0)
 	{
