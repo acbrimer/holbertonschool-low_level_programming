3084f0f2c152b6a8e1242770b9a7a08f8c5aaacf
Adam Brimer <abrimer@mac.com>
2021-03-30 22:21:59 +0000

    Fix for 3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ void copy_files(const char *src_filename, const char *dest_filename)
 	fd_dest = open(dest_filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (fd_dest == -1)
 	{
-		close_fd(fd_src);
+		close(fd_src);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_filename);
 		exit(99);
 	}
@@ -44,8 +44,8 @@ void copy_files(const char *src_filename, const char *dest_filename)
 	{
 		if (buflen <= 0)
 		{
-			close_fd(fd_src);
-			close_fd(fd_dest);
+			close(fd_src);
+			close(fd_dest);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_filename);
 			exit(98);
 		}
