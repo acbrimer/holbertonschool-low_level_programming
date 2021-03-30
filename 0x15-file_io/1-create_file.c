04753d9b304bbae615fdf268af3e4161627223d3
Adam Brimer <abrimer@mac.com>
2021-03-30 20:56:23 +0000

    Added solution for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * create_file - writes text to a new file
+ * @filename: path to new file
+ * @text_content: text to put in the file
+ *
+ * Return: 1 for success, -1 for failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, writelen;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0600);
+	if (fd == -1)
+		return (-1);
+	while (text_content[len])
+		len++;
+	writelen = write(fd, text_content, len);
+	close(fd);
+	if (writelen != len)
+		return (-1);
+
+	return (1);
+}
