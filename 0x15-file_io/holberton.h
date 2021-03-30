79a9bc34bc53b11228ef218bfc9d6c8b51f3ac89
Adam Brimer <abrimer@mac.com>
2021-03-30 23:26:03 +0000

    Updated header with prototypes from methods in 3

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,5 +8,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void copy_files(const char *src_filename, const char *dest_filename);
+void close_fd(int fd);
 #endif
