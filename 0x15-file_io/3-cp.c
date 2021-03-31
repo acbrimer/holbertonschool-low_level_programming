9cae4f21680d8dba254ec30e55363fe4bc311bda
Adam Brimer <abrimer@mac.com>
2021-03-31 00:18:30 +0000

    Fix for 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int ac, char **av)
 	fd_src = open(av[1], O_RDONLY);
 	if (fd_src == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fd_dest = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	fd_dest = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_dest == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	while ((buflen = read(fd_src, buffer, 1024)) > 0)
