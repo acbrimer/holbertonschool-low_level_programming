fb57fe91933e8256bc3e404c45c5fceae54a8cbb
Adam Brimer <abrimer@mac.com>
2021-05-03 23:45:41 +0000

    Fix for 5-- not island if not bordered on all sizes by water

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,8 +4,8 @@
 
 def island_perimeter(grid):
     """Return the perimeter of the island of 1's"""
-    grid_h = len(grid)
-    grid_w = len(grid[0])
+    grid_h = len(grid) - 1
+    grid_w = len(grid[0]) - 1
     all_coords = sum([
         [(x, y, val) for (x, val) in enumerate(row)]
         for (y, row) in enumerate(grid)
@@ -14,12 +14,15 @@ def island_perimeter(grid):
         lambda c: (c[0], c[1]), filter(lambda x: x[2] == 1, all_coords))
     perim = 0
     for c in land_coords:
+        if c[0] == 0 or c[1] == 0 or c[0] == grid_w or c[1] == grid_h:
+            perim = 0
+            break
         if c[1] > 0 and grid[c[1] - 1][c[0]] == 0:
             perim += 1
-        if c[1] < (grid_h - 1) and grid[c[1] + 1][c[0]] == 0:
+        if c[1] < grid_h and grid[c[1] + 1][c[0]] == 0:
             perim += 1
         if c[0] > 0 and grid[c[1]][c[0] - 1] == 0:
             perim += 1
-        if c[0] < (grid_w - 1) and grid[c[1]][c[0] + 1] == 0:
+        if c[0] < grid_w and grid[c[1]][c[0] + 1] == 0:
             perim += 1
     return perim
