92fad1a752248e4f4ab91a90bb492c1c18b1c86f
Adam Brimer <abrimer@mac.com>
2021-07-29 17:19:21 +0000

    Fix for task 0

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -11,20 +11,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *t = malloc(sizeof(hash_table_t));
-	unsigned long int i;
 
 	if (size < 1 || t == NULL)
 		return (NULL);
 
 	t->size = size;
 	t->array = malloc(sizeof(hash_node_t *) * size);
-
-	for (i = 0; i < size; i++)
-	{
-		t->array[i] = malloc(sizeof(hash_node_t));
-		(t->array[i])->key = NULL;
-		(t->array[i])->value = NULL;
-	}
 
 	return (t);
 }
