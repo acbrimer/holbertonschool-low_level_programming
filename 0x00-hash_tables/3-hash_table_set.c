53d98a24003a474b3b49ba8595911b32ffc738f3
Adam Brimer <abrimer@mac.com>
2021-07-29 18:12:14 +0000

    Refactor for 3-- using string.h

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
 /**
  * hash_table_set - sets key in hash table
@@ -13,26 +13,21 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int ix, hash, size = ht->size;
-	int keylen = 0, vallen = 0, i;
-	char *k, *v;
+
+	char *k = malloc(sizeof(char) * strlen(key));
+	char *v = malloc(sizeof(char) * strlen(value));
 	hash_node_t *n = malloc(sizeof(hash_node_t *));
 
-	while (key[keylen])
-		keylen++;
-	while (value[vallen])
-		vallen++;
-	k = malloc(sizeof(char) * (keylen + 1));
-	v = malloc(sizeof(char) * (vallen + 1));
 	if (k == NULL || v == NULL || n == NULL)
 		return (0);
-	for (i = 0; i < keylen; i++)
-		k[i] = key[i];
-	for (i = 0; i < vallen; i++)
-		v[i] = value[i];
+	strcpy(k, key);
+	strcpy(v, value);
 	hash = hash_djb2((const unsigned char *)k);
 	ix = hash % size;
 	n->key = k;
 	n->value = v;
+	if (ht->array[ix])
+		n->next = ht->array[ix];
 	ht->array[ix] = n;
 	return (1);
 }
