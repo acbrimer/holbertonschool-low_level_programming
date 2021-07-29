5bdc1057e8177637b6d6d7facacb63931cdddb77
Adam Brimer <abrimer@mac.com>
2021-07-29 22:36:24 +0000

    Added solution for 4

---
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - gets the value at key for hash table
+ * @ht: pointer to hash table
+ * @key: key to retreive from hash table
+ *
+ * Return: value at key in hash table
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    unsigned long int ix;
+    hash_node_t *node;
+
+    if (ht == NULL || key == NULL)
+        return (NULL);
+    ix = hash_djb2((const unsigned char *)key) % ht->size;
+    if (ht->array[ix]->next == NULL && strcmp(ht->array[ix]->key, key) == 0)
+        return (ht->array[ix]->value);
+    node = malloc(sizeof(hash_node_t));
+    node = memcpy(node, ht->array[ix]->next, sizeof(hash_node_t));
+    while (node)
+    {
+        if (strcmp(node->key, key) == 0)
+            return (node->value);
+        node = node->next;
+    }
+    free(node);
+
+    return (NULL);
+}
