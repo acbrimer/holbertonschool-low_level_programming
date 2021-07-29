c399b7d19ddfb606ab791b043fff602a232737b9
Adam Brimer <abrimer@mac.com>
2021-07-29 18:08:01 +0000

    Fix for 3-- copying strings

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -13,14 +13,26 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int ix, hash, size = ht->size;
+	int keylen = 0, vallen = 0, i;
+	char *k, *v;
 	hash_node_t *n = malloc(sizeof(hash_node_t *));
 
-	if (n == NULL)
+	while (key[keylen])
+		keylen++;
+	while (value[vallen])
+		vallen++;
+	k = malloc(sizeof(char) * (keylen + 1));
+	v = malloc(sizeof(char) * (vallen + 1));
+	if (k == NULL || v == NULL || n == NULL)
 		return (0);
-	hash = hash_djb2((const unsigned char *)key);
+	for (i = 0; i < keylen; i++)
+		k[i] = key[i];
+	for (i = 0; i < vallen; i++)
+		v[i] = value[i];
+	hash = hash_djb2((const unsigned char *)k);
 	ix = hash % size;
-	n->key = (char *)key;
-	n->value = (char *)value;
+	n->key = k;
+	n->value = v;
 	if (ht->array[ix])
 		n->next = ht->array[ix];
 	ht->array[ix] = n;
