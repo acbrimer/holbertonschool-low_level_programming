32ac2f1cb749b732fcaaa62b5c568a6b1f1aa5f0
Adam Brimer <abrimer@mac.com>
2021-07-30 02:56:06 +0000

    Fix for 3

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -31,7 +31,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(tmp->key, key) == 0)
 		{
 			n->next = tmp->next;
+			free(tmp->value);
+			free(tmp->key);
 			free(tmp);
+			break;
 		}
 		tmp = next;
 	}
