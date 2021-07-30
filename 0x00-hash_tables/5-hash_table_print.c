7a15a4258348c30b35abf1a408d7b0c0e59e4193
Adam Brimer <abrimer@mac.com>
2021-07-30 01:35:32 +0000

    Last fix for 5

--- 0x00-hash_tables/5-hash_table_print.c
+++ 0x00-hash_tables/5-hash_table_print.c
@@ -10,14 +10,14 @@
  */
 void print_node(const hash_node_t *n, int isLast)
 {
-	if (n == NULL || n->key == NULL)
-		return;
-	printf("'%s': '%s'", n->key, n->value);
-	if (isLast == 0 || n->next != NULL)
-		printf(", ");
-	if (n->next != NULL)
-		print_node(n->next, isLast);
-	return;
+	if (n != NULL && n->key == NULL)
+	{
+		printf("'%s': '%s'", n->key, n->value);
+		if (isLast == 0 || n->next != NULL)
+			printf(", ");
+		if (n->next != NULL)
+			print_node(n->next, isLast);
+	}
 }
 
 /**
@@ -28,13 +28,14 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, lastIx = 0;
 
-	if (ht == NULL)
-		return;
-	for (i = 0; i < ht->size; i++)
-		if (ht->array[i] != NULL && ht->array[i]->key != NULL)
-			lastIx = i;
-	printf("{");
-	for (i = 0; i < ht->size; i++)
-		print_node(ht->array[i], i == lastIx ? 1 : 0);
-	printf("}\n");
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+			if (ht->array[i] != NULL && ht->array[i]->key != NULL)
+				lastIx = i;
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+			print_node(ht->array[i], i == lastIx ? 1 : 0);
+		printf("}\n");
+	}
 }
